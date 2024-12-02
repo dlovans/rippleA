57bf65fc9e807aeb7c1b0e786d5303fc50fc490a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-02 16:46:35 +0100

    Initial commit

---
+++ app/src/main/java/dev/dlovan/ripplea/MainActivity.kt
@@ -0,0 +1,25 @@
+package dev.dlovan.ripplea
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.dlovan.ripplea.ui.views.ChatScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    ChatScreen()
+}
