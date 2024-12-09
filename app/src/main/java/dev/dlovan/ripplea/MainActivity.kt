4f66710027e20232c8f3b0e0aafbb77e16b7b075
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-09 20:38:36 +0100

    Set content to application nav host

--- app/src/main/java/dev/dlovan/ripplea/MainActivity.kt
+++ app/src/main/java/dev/dlovan/ripplea/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dev.dlovan.ripplea.views.ChatScreen
+import dev.dlovan.ripplea.views.RippleNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ChatScreen()
+            RippleNavHost()
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
