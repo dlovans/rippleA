a6daac7ef8e2c647e0ee5bca5eb115a7c6e5f374
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-09 20:38:18 +0100

    Add colors to Color.kt

--- app/src/main/java/dev/dlovan/ripplea/ui/theme/Color.kt
+++ app/src/main/java/dev/dlovan/ripplea/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Emerald = Color(red = 52, green = 211, blue = 153)
+val Stone = Color(red = 54, green = 69, blue = 79)
