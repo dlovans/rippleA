4d3812c89d8402309f35161b8009c63d4d8eb9a6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-20 11:45:12 +0100

    Remove FirebaseManager singleton

--- app/src/main/java/dev/dlovan/ripplea/RippleApplication.kt
+++ app/src/main/java/dev/dlovan/ripplea/RippleApplication.kt
@@ -1,14 +1,7 @@
 package dev.dlovan.ripplea
 
 import android.app.Application
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-import javax.inject.Singleton
 
 @HiltAndroidApp
 class RippleApplication: Application() {
@@ -16,10 +9,3 @@ class RippleApplication: Application() {
         super.onCreate()
     }
 }
-
-@Singleton
-class FirebaseManager @Inject constructor() {
-    val auth: FirebaseAuth = Firebase.auth
-    val db: FirebaseFirestore = Firebase.firestore
-}
-
